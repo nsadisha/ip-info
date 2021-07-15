25d8a03e39a6abe3c795ff7b32807afb0662f08f
nsadisha <nsadisha@gmail.com>
2021-07-16 01:51:52 +0530

    main routs are created

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
+const ipl = require('ip-locator')
 const port = 3000
 
 app.get('/', (req, res) => {
-  var ip = req.connection.remoteAddress
-  res.json({"status":"success","message":"Hello World!","data":ip})
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
+  ipl.getDomainOrIPDetails(ip,'json', function (err, data) {
+    res.json(data)
+  })
+})
+app.get('/:ip', (req, res) => {
+  ipl.getDomainOrIPDetails(req.params.ip,'json', function (err, data) {
+    res.json(data)
+  })
 })
 
 app.listen(port, () => {
