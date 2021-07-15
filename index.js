2d19c5ebd0a284f6e767ac0888125352b78419b9
nsadisha <nsadisha@gmail.com>
2021-07-16 02:24:14 +0530

    completed with serverless-http

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const express = require('express')
 const app = express()
+const router = express.Router()
 const ipl = require('ip-locator')
+const serverless = require('serverless-http')
 const port = 3000
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
   ipl.getDomainOrIPDetails(ip,'json', function (err, data) {
     res.json(data)
   })
 })
-app.get('/:ip', (req, res) => {
+router.get('/:ip', (req, res) => {
   ipl.getDomainOrIPDetails(req.params.ip,'json', function (err, data) {
     res.json(data)
   })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.use('.netlify/functions/index', router)
+module.exports.handler = serverless(app)
+
+//app.listen(port, () => {
+//  console.log(`Example app listening at http://localhost:${port}`)
+//})
