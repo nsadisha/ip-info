5c2bab763f4b443f43c36101fb6d5f33bd4fee6e
nsadisha <nsadisha@gmail.com>
2021-07-16 11:40:06 +0530

    modified

--- src/api.js
+++ src/api.js
@@ -2,19 +2,18 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const ipl = require('ip-locator')
-var ip = require('ip');
 const serverless = require('serverless-http')
 
-router.get('/', (req, res) => {
-  var myip = ip.address()
+router.get('/',(req, res, next) => {
+  var myip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
   ipl.getDomainOrIPDetails(myip,'json', function (err, data) {
     res.json(data)
   })
 })
-router.get('/ip', (req, res) => {
-  var myip = ip.address()
-  res.json({"status": "success", "ip": ip})
+router.get('/ip',(req, res, next) => {
+  var myip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  res.json({"status": "success", "date": Date(), "ip": myip})
 })
 router.get('/:ip', (req, res) => {
   ipl.getDomainOrIPDetails(req.params.ip,'json', function (err, data) {
