0b4dbffb4f5b4aaaccdae7eb6ef116e91c735a57
nsadisha <nsadisha@gmail.com>
2021-07-16 11:46:27 +0530

    netlify functions removed

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const express = require('express')
 const app = express()
-const router = express.Router()
 const ipl = require('ip-locator')
-const serverless = require('serverless-http')
 
-router.get('/',(req, res, next) => {
+app.get('/',(req, res, next) => {
   var myip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
   ipl.getDomainOrIPDetails(myip,'json', function (err, data) {
     res.json(data)
   })
 })
-router.get('/ip',(req, res, next) => {
+app.get('/ip',(req, res, next) => {
   var myip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
   res.json({"status": "success", "date": Date(), "ip": myip})
 })
-router.get('/:ip', (req, res) => {
+app.get('/:ip', (req, res) => {
   ipl.getDomainOrIPDetails(req.params.ip,'json', function (err, data) {
     res.json(data)
   })
 })
 
-app.use('/.netlify/functions/api', router)
-module.exports.handler = serverless(app)
+app.listen(process.env.port || 3000, ()=>{
+  console.log("listening for requests")
+})
