542cbe1d9fad2cdd0e93e914bb851288e250f0ab
nsadisha <nsadisha@gmail.com>
2021-07-16 23:19:37 +0530

    testing

--- index.js
+++ index.js
@@ -2,21 +2,8 @@ const express = require('express')
 const app = express()
 const ipl = require('ip-locator')
 
-app.get('/',(req, res, next) => {
-  var myip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-
-  ipl.getDomainOrIPDetails(myip,'json', function (err, data) {
-    res.json(data)
-  })
-})
-app.get('/ip',(req, res, next) => {
-  var myip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-  res.json({"status": "success", "date": Date(), "ip": myip})
-})
-app.get('/:ip', (req, res) => {
-  ipl.getDomainOrIPDetails(req.params.ip,'json', function (err, data) {
-    res.json(data)
-  })
+app.get('/', (req, res) => {
+  res.json({"Hello": "hi"})
 })
 
 app.listen(process.env.port || 3000, ()=>{
